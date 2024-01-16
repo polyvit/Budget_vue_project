6cb35bfd42bbdf628fe9aa4e14fe156e4415bc43
Polina Limonova <polina98_08@mail.ru>
2024-01-16 23:28:16 +0300

    Task 6

--- src/components/BudgetList.vue
+++ src/components/BudgetList.vue
@@ -1,8 +1,17 @@
 <template>
   <div class="burger-list-wrap">
     <ElCard :header="header">
+      <div class="filters">
+        <ElButton plain @click="filterList('ALL')" @keydown="filterList('ALL')">All</ElButton>
+        <ElButton plain @click="filterList('INCOME')" @keydown="filterList('INCOME')"
+          >Incomes</ElButton
+        >
+        <ElButton plain @click="filterList('OUTCOME')" @keydown="filterList('OUTCOME')"
+          >Outcomes</ElButton
+        >
+      </div>
       <template v-if="!isEmpty">
-        <BudgetListItem :list="list" @deleteItem="deleteItem" />
+        <BudgetListItem :list="filteredList" @deleteItem="deleteItem" />
       </template>
       <ElAlert v-else type="info" title="This list is empty" :closable="false" />
     </ElCard>
@@ -20,7 +29,11 @@ export default {
   },
   data: () => ({
     header: "List of my budget",
+    filteredList: {},
   }),
+  created() {
+    this.filterList("ALL");
+  },
   props: {
     list: {
       type: Object,
@@ -31,10 +44,28 @@ export default {
     isEmpty() {
       return !Object.keys(this.list).length;
     },
+    // filteredList: {
+    //   get() {
+    //     return this.list;
+    //   },
+    //   set(data) {
+    //     this.filteredList = data;
+    //   },
+    // },
   },
   methods: {
     deleteItem(id) {
       this.$emit("deleteItem", id);
+    },
+    filterList(type) {
+      const listArr = Object.values(this.list);
+      if (type === "ALL") {
+        this.filteredList = this.list;
+      } else {
+        const arrayEl = listArr.filter((item) => item.type === type);
+        this.filteredList = {};
+        arrayEl.forEach((item) => (this.filteredList[item.id] = item));
+      }
     },
   },
 };
@@ -50,5 +81,8 @@ export default {
 .budget-value {
   font-weight: bold;
   margin-right: 15px;
+}
+.filters {
+  margin-bottom: 15px;
 }
 </style>
